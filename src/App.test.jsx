3e87037b96df094630b26372a56741a2ecf87115
nkrpradhan <nkrpradhan@gmail.com>
2023-10-29 12:20:52 +0000

    feat: test files

---
+++ src/App.test.jsx
@@ -0,0 +1,12 @@
+import { describe, it } from "vitest";
+import { render, screen } from "@testing-library/react";
+
+import App from "./App";
+
+describe("App", () => {
+  it("renders app", () => {
+    render(<App></App>);
+
+    expect(screen.getByText(/Vite/i)).toBeInTheDocument();
+  });
+});
